93ac1e27ff307faee48806f6b25705e87c9cadf1
ekim49 <hailieejkim@gmail.com>
2023-03-29 23:27:17 +0900

    remove .env file from git

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,11 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import KakaoProvider from 'next-auth/providers/kakao';
 import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/lib/prisma';
 
 export default NextAuth({
+	// adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
@@ -18,4 +21,29 @@ export default NextAuth({
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
 		}),
 	],
+	pages: {
+		signIn: '/login',
+	},
+	// callbacks: {
+	// 	async signIn({ user, account, profile, email, credentials }) {
+	// 		if (user) {
+	// 			console.log('user는: ', user);
+	// 			console.log('account는: ', account);
+	// 			console.log('profile는: ', profile);
+	// 			console.log('email은: ', email);
+	// 			console.log('credentials는: ', credentials);
+	// 		} else {
+	// 			console.log('없음');
+	// 		}
+	// 	},
+	// 	async session({ session, token, user }) {
+	// 		// if (session) {
+	// 		console.log('session은: ', session);
+	// 		console.log('token은: ', token);
+	// 		console.log('user는: ', user);
+	// 		// } else {
+	// 		// 	console.log('session 없다');
+	// 		// }
+	// 	},
+	// },
 });
