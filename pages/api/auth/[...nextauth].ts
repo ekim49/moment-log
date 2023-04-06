f981cbdd816e2231e2dcf2942ae2b15b9afa6ffe
ekim49 <hailieejkim@gmail.com>
2023-04-06 19:10:54 +0900

    refactor: 사용하지 않는 import문 제거 및 주석 삭제

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,9 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '../../../lib/prisma/index';
 import GoogleProvider from 'next-auth/providers/google';
 import KakaoProvider from 'next-auth/providers/kakao';
 import GitHubProvider from 'next-auth/providers/github';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../lib/prisma/index';
-import { getUserById } from '@/lib/prisma/users';
 
 export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma),
@@ -25,34 +24,6 @@ export const authOptions: NextAuthOptions = {
 	pages: {
 		signIn: '/login',
 	},
-	// callbacks: {
-	// 	async signIn({ user, account, profile, email, credentials }) {
-	// 		if (user) {
-	// 			console.log('user는: ', user);
-	// 			console.log('account는: ', account);
-	// 			console.log('profile는: ', profile);
-	// 			console.log('email은: ', email);
-	// 			console.log('credentials는: ', credentials);
-	// 		} else {
-	// 			console.log('없음');
-	// 		}
-	// 	},
-	// 	async session({ session, token, user }) {
-	// 		// if (session) {
-	// 		console.log('session은: ', session);
-	// 		console.log('token은: ', token);
-	// 		console.log('user는: ', user);
-	// 		// } else {
-	// 		// 	console.log('session 없다');
-	// 		// }
-	// 	},
-	// },
-	// callbacks: {
-	// 	async signIn({ user }) {
-	// 		const registeredUser = await getUserById(user.id);
-	// 		return registeredUser ? true : '/signup';
-	// 	},
-	// },
 };
 
 export default NextAuth(authOptions);
